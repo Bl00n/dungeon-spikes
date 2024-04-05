4ec345e7e11a462ab1846202c84fc977c9ea021b
Bl00n <141494652+Bl00n@users.noreply.github.com>
2024-04-05 13:13:09 +0100

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,208 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b f f 1 f f b b b f f 1 f f b 
+b f f 1 1 1 f f b f f 1 1 1 f f 
+b b f f f f f b b b f f f f f b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b f f 1 f f b b b f f 1 f f b 
+b f f 1 1 1 f f b f f 1 1 1 f f 
+b b f f f f f b b b f f f f f b 
+`, img`
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b f 1 1 1 f b b b f 1 1 1 f b 
+b f f 1 1 1 f f b f f 1 1 1 f f 
+b b f f f f f b b b f f f f f b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b f 1 1 1 f b b b f 1 1 1 f b 
+b f f 1 1 1 f f b f f 1 1 1 f f 
+b b f f f f f b b b f f f f f b 
+`, img`
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b b 1 1 1 b b b b b 1 1 1 b b 
+b b f 1 1 1 f b b b f 1 1 1 f b 
+b f 1 1 1 1 1 f b f 1 1 1 1 1 f 
+b b f f f f f b b b f f f f f b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b b 1 1 1 b b b b b 1 1 1 b b 
+b b f 1 1 1 f b b b f 1 1 1 f b 
+b f 1 1 1 1 1 f b f 1 1 1 1 1 f 
+b b f f f f f b b b f f f f f b 
+`, img`
+b b b b b b b b b b b b b b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b b 1 1 1 b b b b b 1 1 1 b b 
+b b b 1 1 1 b b b b b 1 1 1 b b 
+b b 1 1 1 1 1 b b b 1 1 1 1 1 b 
+b f 1 1 1 1 1 f b f 1 1 1 1 1 f 
+b b f f f f f b b b f f f f f b 
+b b b b b b b b b b b b b b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b b 1 1 1 b b b b b 1 1 1 b b 
+b b b 1 1 1 b b b b b 1 1 1 b b 
+b b 1 1 1 1 1 b b b 1 1 1 1 1 b 
+b f 1 1 1 1 1 f b f 1 1 1 1 1 f 
+b b f f f f f b b b f f f f f b 
+`, img`
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b b 1 1 1 b b b b b 1 1 1 b b 
+b b f 1 1 1 f b b b f 1 1 1 f b 
+b f 1 1 1 1 1 f b f 1 1 1 1 1 f 
+b b f f f f f b b b f f f f f b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b b 1 1 1 b b b b b 1 1 1 b b 
+b b f 1 1 1 f b b b f 1 1 1 f b 
+b f 1 1 1 1 1 f b f 1 1 1 1 1 f 
+b b f f f f f b b b f f f f f b 
+`, img`
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b f 1 1 1 f b b b f 1 1 1 f b 
+b f f 1 1 1 f f b f f 1 1 1 f f 
+b b f f f f f b b b f f f f f b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b f 1 1 1 f b b b f 1 1 1 f b 
+b f f 1 1 1 f f b f f 1 1 1 f f 
+b b f f f f f b b b f f f f f b 
+`, img`
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b f f 1 f f b b b f f 1 f f b 
+b f f 1 1 1 f f b f f 1 1 1 f f 
+b b f f f f f b b b f f f f f b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b 1 b b b b b b b 1 b b b 
+b b f f 1 f f b b b f f 1 f f b 
+b f f 1 1 1 f f b f f 1 1 1 f f 
+b b f f f f f b b b f f f f f b 
+`, img`
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b f f 1 f f b b b f f 1 f f b 
+b f f f 1 f f f b f f f 1 f f f 
+b b f f f f f b b b f f f f f b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b f f 1 f f b b b f f 1 f f b 
+b f f f 1 f f f b f f f 1 f f f 
+b b f f f f f b b b f f f f f b 
+`, img`
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b f f f f f b b b f f f f f b 
+b f f f 1 f f f b f f f 1 f f f 
+b b f f f f f b b b f f f f f b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b f f f f f b b b f f f f f b 
+b f f f 1 f f f b f f f 1 f f f 
+b b f f f f f b b b f f f f f b 
+`, img`
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b f f f f f b b b f f f f f b 
+b f f f f f f f b f f f f f f f 
+b b f f f f f b b b f f f f f b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b b b b b b b b b b b b b b b 
+b b f f f f f b b b f f f f f b 
+b f f f f f f f b f f f f f f f 
+b b f f f f f b b b f f f f f b 
+`];
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
